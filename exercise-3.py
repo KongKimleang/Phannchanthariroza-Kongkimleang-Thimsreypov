b86deb020742e36594f7116a4166d13fe185f6b7
KongKimleang <156416030+KongKimleang@users.noreply.github.com>
2024-04-07 23:08:12 +0700

    exercise-3.py

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,18 @@
+
 def remove_all_after(numbers, n):
-    ...
+    if n in numbers:
+        index = numbers.index(n)
+        return numbers[:index+1]
+    else:
+        return numbers
+
+# User  input
+print("Enter the elements of the list, separated by spaces:")
+numbers_input = input()
+numbers = list(map(int, numbers_input.split()))
+
+print("Enter the border element:")
+n = int(input())
+
+result = remove_all_after(numbers, n)
+print(result)
